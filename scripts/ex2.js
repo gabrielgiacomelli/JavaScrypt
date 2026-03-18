1fd68c2f93db5aa369da1d43178bb320c1423188
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-03-18 19:21:18 -0300

    add - new button

---
+++ scripts/ex2.js
@@ -0,0 +1,9 @@
+function ContagemRegressiva() {
+    alert("Prepare-se para a contagem!");
+
+    for (let i = 10; i >= 0; i--) {
+        alert("Contagem: " + i);
+    }
+
+    alert("Lançamento Realizado!");
+}
