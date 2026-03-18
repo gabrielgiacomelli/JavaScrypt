c223b9eb4f2aa32e92e2cbf9dd7d258915504790
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-03-18 12:51:49 -0300

    add - especific button

---
+++ scripts/ex1.js
@@ -0,0 +1,20 @@
+function somarNumeros() {
+    let soma = 0;
+    let continuar = true;
+
+    while (continuar) {
+
+        let numero = parseFloat(prompt("Digite um número para somar: "));
+
+        if (!isNaN(numero)) {
+
+            soma += numero;
+            console.log(soma);
+            
+        } else {
+            alert("Por Favor, digite um número válido!");
+        }
+        continuar = confirm("Deseja adicionar mais um número?");
+    }
+    alert("A soma dos número é " + soma);
+}
