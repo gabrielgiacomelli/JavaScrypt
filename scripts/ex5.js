fb639fcdba2b87b5c65ea3b72f834d16c1db6e64
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-03-23 09:55:24 -0300

    up - +=

--- scripts/ex5.js
+++ scripts/ex5.js
@@ -2,7 +2,7 @@ function inteiro() {
     let numero = parseInt(prompt("Informe um número inteiro: "));
 
     if (numero % 2 === 0) {
-        numero = numero + 1; 
+        numero += 1; 
         console.log("O número era par e agora está ímpar: " + numero);
     } else {
         numero = numero + 1; 
