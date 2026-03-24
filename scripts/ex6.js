9f23f7cf26e82770afcfed33c9170dedd6e8959e
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-03-24 15:58:56 -0300

    up - prompt

--- scripts/ex6.js
+++ scripts/ex6.js
@@ -1,5 +1,5 @@
 function LETRA() {
-    let letra = prompt("Digite uma letra");
+    let letra = prompt("Digite uma letra").toLowerCase();
      if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
         alert("Sua letra é uma vogal");
     } else {
